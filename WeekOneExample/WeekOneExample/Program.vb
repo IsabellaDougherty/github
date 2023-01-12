a2e316d00f374e8306edb9adc136ee7d85420f3f
Isabella <belladougherty@gmail.com>
2023-01-12 08:46:10 -0700

    Added Console.Beep song from Tim's ConsolFun Repository

--- WeekOneExample/WeekOneExample/Program.vb
+++ WeekOneExample/WeekOneExample/Program.vb
@@ -1,7 +1,20 @@
+'Isabella Dougherty
+'RCET0265
+'Spring 2023
+'Getting Started Examples
+'
+
 Imports System
 
 Module Program
     Sub Main(args As String())
-        Console.WriteLine("Hello World!")
+        'Console.WriteLine()
+        'Console.Write("Everything is going to be okay <3")
+        'Beep can have a frequency and a durration
+        Console.Beep(392, 500) 'G4'
+        Console.Beep(440, 500) 'A4'
+        Console.Beep(349, 750) 'F4'
+        Console.Beep(196, 750) 'G3'
+        Console.Beep(262, 900) 'C4'
     End Sub
 End Module
