78cd2a4bd8b9ae202616096baa633e754b0dfccb
Isabella <belladougherty@gmail.com>
2023-01-17 08:26:26 -0700

    Created notes of how to do addition, multiplication, division, and Mod

---
+++ S23-Math/S23-Math/Program.vb
@@ -0,0 +1,33 @@
+Imports System
+
+Module Operators
+    Sub Main()
+        'Writes a string that says 5+5=
+        Console.Write("5 + 5 = ")
+        'Use the console to add 5 + 5
+        Console.WriteLine(5 + 5)
+        'Adds a blank line
+        Console.WriteLine()
+        'Example of muliplication
+        Console.Write("5 * 5 = ")
+        Console.WriteLine(5 * 5)
+        Console.WriteLine()
+        'Example of exponent use
+        Console.Write("5 to the power of 2 = ")
+        Console.WriteLine(5 ^ 2)
+        Console.WriteLine()
+        'Two examples of division, can use a forward or backwards slash
+        Console.Write("5 / 2 = ")
+        Console.WriteLine(5 / 2)
+        Console.WriteLine()
+        Console.Write("5 \ 2 = ")
+        Console.WriteLine(5 \ 2)
+        Console.WriteLine()
+        'example of a Mod operator
+        Console.Write("5 Mod 2 = ")
+        Console.WriteLine(5 Mod 2)
+        Console.WriteLine()
+        Console.Read() 'Wait for user response here
+    End Sub
+
+End Module
