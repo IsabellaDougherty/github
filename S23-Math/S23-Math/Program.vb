049833ab74bef2e7c1724a12ebbe1e6810419a94
Isabella <belladougherty@gmail.com>
2023-01-17 08:59:27 -0700

    Added String Concationation, Comparison Operators, Assignment Operators, and learned how to use and If Then Else statement (even though that hasn't been gone over in class yet)

--- S23-Math/S23-Math/Program.vb
+++ S23-Math/S23-Math/Program.vb
@@ -1,4 +1,5 @@
 Imports System
+Imports System.Linq.Expressions
 
 Module Operators
     Sub Main()
@@ -27,7 +28,39 @@ Module Operators
         Console.Write("5 Mod 2 = ")
         Console.WriteLine(5 Mod 2)
         Console.WriteLine()
-        Console.Read() 'Wait for user response here
+
+        'String concationation
+        Console.WriteLine("apple" & " " & "banana")
+        Console.WriteLine()
+
+        'Comparison Operators
+        'Returns a boolean for whether values are true or false
+        Console.Write("Is 5 Greater Than 6? ")
+        Console.WriteLine(5 > 6)
+        Console.WriteLine()
+        Console.Write("Is 5 Less Than 6? ")
+        Console.WriteLine(5 < 6)
+        Console.WriteLine()
+        Console.Write("Is 5 equal to 6? ")
+        Console.WriteLine(5 = 6)
+        Console.WriteLine()
+        'First value is not equal to the second value
+        Console.Write("Is 5 not 6? ")
+        Console.WriteLine(5 <> 6)
+        Console.WriteLine()
+
+        'Assignment Operators
+        Dim num As Integer
+        Dim fruit As String
+        num = 5
+        If (num = 5) Then fruit = "apple"
+        Console.WriteLine("You have selected " & fruit & " which corrolates to the number " & Str(num))
+        'Number sign indicates the string will have variables inside it
+        Console.WriteLine($"You have {num} {fruit}(s)")
+        Console.WriteLine()
+        Console.WriteLine("What is your favorite Fruit? Hit 'enter' when complete! ")
+        fruit = Console.ReadLine() 'Wait for user response here
+        Console.WriteLine($"You have entered your favorite fruit as {fruit}!")
     End Sub
 
 End Module
