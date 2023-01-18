77be33cc757b5069d43fab0fd5657aa12fcb8d3c
Isabella <belladougherty@gmail.com>
2023-01-17 20:17:11 -0700

    Created a simple calculator that can take two numbers and add, subtract, multiply, and divide them then return the answer as a decimal value as needed.

---
+++ Simple Calculator/Program.vb
@@ -0,0 +1,25 @@
+Imports System
+
+'Isabella Dougherty
+'RCET0265
+'Spring 2023
+'Simple Calculator
+Module Operations
+    Sub Main()
+        Dim first As Integer
+        Dim second As Integer
+        Dim answer As Decimal
+        Dim action As String
+        Console.WriteLine("Enter your first number: ")
+        first = Console.ReadLine()
+        Console.WriteLine("Enter your second number: ")
+        second = Console.ReadLine()
+        Console.WriteLine("Do you want a sum, difference, product, or quotient? ")
+        action = Console.ReadLine()
+        If action = "sum" Then answer = first + second
+        If action = "product" Then answer = first * second
+        If action = "difference" Then answer = first - second
+        If action = "quotient" Then answer = first / second
+        Console.WriteLine($"The solution is {answer}")
+    End Sub
+End Module
