2979a3b3917bb66d5b48b4590ec16349d9983dd4
Isabella <belladougherty@gmail.com>
2023-01-19 08:26:20 -0700

    Corrected If statements to a series of If ElseIf statements.

--- Simple Calculator/Program.vb
+++ Simple Calculator/Program.vb
@@ -10,6 +10,7 @@ Module Operations
         Dim first As Decimal
         Dim second As Decimal
         Dim answer As Decimal
+        Dim viable As Boolean
         Dim action As String
 
         'Gathering User Inputs and assigning inputs to variables
@@ -23,14 +24,37 @@ Module Operations
 Please use all lowercase and spell your response")
         action = Console.ReadLine()
 
-        'The following takes the inputs and uses If statements to determine what function was chosen then calculates
-        If action = "sum" Then answer = first + second
-        If action = "product" Then answer = first * second
-        If action = "difference" Then answer = first - second
-        If action = "quotient" Then answer = first / second
+        'The following takes the inputs and uses If Then Else statements to determine what function was chosen then calculates
+        If action = "sum" Then
+            answer = first + second
+            viable = True
 
+        ElseIf action = "product" Then
+            answer = first * second
+            viable = True
+
+        ElseIf action = "difference" Then
+            answer = first - second
+
+        ElseIf action = "quotient" Then
+            answer = first / second
+            viable = True
+
+        Else
+            Console.WriteLine("The input value is innaplicable")
+            viable = False
+        End If
+
+        Console.WriteLine()
+
+        'Checks to see if a calculation was actually made
         'The simplified as will round the decimal value of a quotient to the nearest whole.
         'Outputs the answer
+        If viable Then
         Console.WriteLine($"The solution is {answer}, simplified as {CInt(answer)}")
+        Else
+            Console.WriteLine("You have input an invalid action, make sure you only use lower case letters and spell the
+action exactly as shown above")
+        End If
     End Sub
 End Module
