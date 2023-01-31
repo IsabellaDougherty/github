1f205fedf0c942f10770e64bb3c97c56306f63c9
Isabella <belladougherty@gmail.com>
2023-01-31 08:45:43 -0700

    Created the code, made a case statement to check the random number given and return one of 11 different wisdoms

--- RandomNumbers/ConsoleApp1/RandomNumber.vb
+++ RandomNumbers/ConsoleApp1/RandomNumber.vb
@@ -1,3 +1,6 @@
+Option Explicit On
+'Option Strict On
+
 Imports System
 
 Module RandomNumber
@@ -12,11 +15,24 @@ Module RandomNumber
         _randomNumber = Int(temp)
         Console.WriteLine(temp)
         Console.WriteLine(_randomNumber)
+        For i = 0 To 10
+            Console.WriteLine(RandomValue())
+        Next
     End Sub
+
+    Function RandomValue() As Integer
+        Dim _randomNumber As Integer
+        Dim rangeMax As Integer = 10
+
+        Randomize()
+        _randomNumber = Int(Rnd() * rangeMax)
+        Return _randomNumber
+    End Function
 
     Function RandomNumber() As Integer
         'Makes a variable equivalent to a random number (that appears as a decimal) multiplied by 100 and then with 1 add to give a range of random numbers
         'from 1 to 100
         Dim number As Integer = CInt(100 * Rnd() + 1)
+        Return number
     End Function
 End Module
