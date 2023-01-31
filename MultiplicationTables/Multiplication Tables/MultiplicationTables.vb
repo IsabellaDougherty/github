50edb3276e6b372c530113b2ea7348e5f4150705
Isabella <belladougherty@gmail.com>
2023-01-31 08:54:28 -0700

    Created code and added header, and changed code name

---
+++ MultiplicationTables/Multiplication Tables/MultiplicationTables.vb
@@ -0,0 +1,13 @@
+'Isabella Dougherty
+'RCET0265
+'Spring 2023 
+'Multiplication Tables
+'
+
+Imports System
+
+Module MultiplicationTables
+    Sub Main(args As String())
+        Console.WriteLine("Hello World!")
+    End Sub
+End Module
