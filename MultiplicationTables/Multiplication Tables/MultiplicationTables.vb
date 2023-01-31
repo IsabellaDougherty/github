1a7fbb0a5f76d0303ccd8ddc6cff191d7e0193f8
Isabella <belladougherty@gmail.com>
2023-01-31 08:57:24 -0700

    Got rid of the hello world base code

--- MultiplicationTables/Multiplication Tables/MultiplicationTables.vb
+++ MultiplicationTables/Multiplication Tables/MultiplicationTables.vb
@@ -2,12 +2,12 @@
 'RCET0265
 'Spring 2023 
 'Multiplication Tables
-'
+'https://github.com/IsabellaDougherty/github.git
 
 Imports System
 
 Module MultiplicationTables
-    Sub Main(args As String())
-        Console.WriteLine("Hello World!")
+    Sub Main()
+
     End Sub
 End Module
