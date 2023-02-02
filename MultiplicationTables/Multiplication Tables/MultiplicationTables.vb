972e605f4031d47a02ec9a3607646936e022da62
Isabella <belladougherty@gmail.com>
2023-02-01 18:52:22 -0700

    Rewrote code to actually work, utilized the String.Format() to organize the table

--- MultiplicationTables/Multiplication Tables/MultiplicationTables.vb
+++ MultiplicationTables/Multiplication Tables/MultiplicationTables.vb
@@ -7,9 +7,24 @@
 Option Explicit On
 
 Imports System
-
 Module MultiplicationTables
-    Sub Main()
 
+    Sub Main()
+        ' Get user input for number
+        Console.Write("Enter a number: ")
+        Dim num As Integer = Console.ReadLine()
+
+        ' Display multiplication table
+        Console.WriteLine("Multiplication Table:")
+        For i As Integer = 1 To num
+            For j As Integer = 1 To num
+                Console.Write(String.Format("{0, 4}", i * j))
+            Next
+            Console.WriteLine()
+        Next
     End Sub
+
 End Module
+
+
+
