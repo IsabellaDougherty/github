4cac4b191270d3af84e8db3169fdd1c59a805a43
IsabellaDougherty <belladougherty@isu.edu>
2023-02-08 21:36:09 -0700

    Created a loop to iterate through and assign a count to the first array and two rolls added to the second dimension of the array. Then created a second loop to itterate through and print out all the values

---
+++ RollOfTheDice.vb
@@ -0,0 +1,29 @@
+Imports System
+
+Module RollOfTheDice
+    Sub Main()
+        Dim count(1000, 1000) As Integer
+        Dim proxyCount As Integer
+        Dim proxyRolls As Integer
+        Dim random As New Random()
+        Dim roll As Integer
+
+        For i = 0 To 999
+            proxyCount += 1
+            count(i, 0) = proxyCount
+            roll = random.Next(1, 6)
+            proxyRolls += roll
+            roll = random.Next(1, 6)
+            proxyRolls += roll
+            count(i, 1) = proxyRolls
+        Next
+
+        Console.WriteLine("~Random Rolls~")
+        For i = 0 To 999
+            For j = 0 To 1
+                Console.Write(count(i, j) & " | ")
+            Next
+            Console.WriteLine()
+        Next
+    End Sub
+End Module
